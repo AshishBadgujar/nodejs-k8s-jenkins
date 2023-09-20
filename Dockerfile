4c94a195414891654ea9c40f0cd25be71d2cc7d0
Ashishbadgujar <abadgujar@nvidia.com>
2023-09-20 11:28:25 +0530

    bug: solved

--- Dockerfile
+++ Dockerfile
@@ -7,7 +7,8 @@ COPY ./app/package.json /home/app
 # will execute npm install in /home/app because of WORKDIR
 RUN npm install
 
-COPY ./app /home/app
+COPY . /home
 EXPOSE 3000
 # no need for /home/app/server.js because of WORKDIR
+
 CMD ["npm", "start"]
--- app/index.js
+++ app/index.js
@@ -2,9 +2,10 @@ const express = require('express')
 
 const app = express()
 const PORT = 3000
-app.route('/', (req, res) => {
+
+app.get('/', (req, res) => {
     console.log("hello world")
-    res.send("Hello world")
+    return res.send("Hello world");
 })
 
 app.listen(PORT, (err) => {
